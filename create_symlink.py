a323fdb10a0b7e41f4b4efd92c2e8dd5793149a2
carl <kinghwaking@163.com>
2023-03-22 14:03:46 +0800

    更新，修复无法删除空文件夹的错误。

--- create_symlink.py
+++ create_symlink.py
@@ -34,16 +34,20 @@ tk.Button(root, text="选择文件夹", command=lambda: select_folder(folder2_en
 def create_symlink():
     src = folder1_entry.get()
     dest = folder2_entry.get()
-    if os.path.exists(src):
-        if os.path.exists(dest):
-            if os.listdir(dest):
-                result_label.config(text="目标文件夹存非空", fg="red")
-            else:
-                os.remove(dest)
-        os.symlink(src, dest)
-        result_label.config(text="软链接创建成功！", fg="green")
-    else:
-        result_label.config(text="输入的文件夹路径不存在，请重新输入。", fg="red")
+    try:
+        if os.path.exists(src):
+            if os.path.exists(dest):
+                if os.listdir(dest):
+                    result_label.config(text="目标文件夹存非空", fg="red")
+                else:
+                    os.rmdir(dest)
+            os.symlink(src, dest)
+            result_label.config(text="软链接创建成功！", fg="green")
+        else:
+            result_label.config(text="输入的文件夹路径不存在，请重新输入。", fg="red")
+    except Exception as e:
+        result_label.config(text=e, fg="red")
+        raise e
 
 tk.Button(root, text="创建软链接", command=create_symlink).grid(row=2, column=1, padx=10, pady=10)
 
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # 软链接创建程序
 
 > 如何打包：pyinstaller -F --uac-admin create_symlink.py
+
+## 如何使用程序
+
+下载`create_symlink.exe`，双击并允许管理员权限运行。
+
+## 目前尚未修复的BUG
+
+N/A
+
